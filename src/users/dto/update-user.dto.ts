946fa9cb46eb3667decd9e1ac27b5a52f510d1f3
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-06-01 19:50:57 +0330

    feat(user): implementing dtos for creating  and updating users

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prettier/prettier */
+
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsLowercase,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { WishlistItemDto } from './whishlist.dto';
+
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)/;
+
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  firstname?: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  lastname?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsLowercase({ message: 'Username must be lowercase' })
+  @Type(() => String)
+  username?: string; // Be careful allowing username updates due to uniqueness
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8, { message: 'Password must have 8 or more characters' })
+  @Matches(passwordRegex, {
+    message: 'Password must have at least one letter and one digit',
+  })
+  password?: string; // Handle password updates carefully (e.g., current password check)
+
+  @IsPhoneNumber('IR')
+  @IsNotEmpty({ message: 'Phone number is required' })
+  @Type(() => String)
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  address?: string;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => WishlistItemDto)
+  wishlist?: WishlistItemDto[];
+}
